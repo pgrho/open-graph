46a6db6c1715acdea17f111eccafc93bd9c72061
pgrho <shipwreck@live.jp>
2017-08-18 22:24:44 +0900

    Added MusicSongAlbumTest

---
+++ src/Shipwreck.OpenGraph.Tests/MusicSongAlbumTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Shipwreck.OpenGraph
+{
+    public class MusicSongAlbumTest
+    {
+        [Fact]
+        public void DiscTest()
+        {
+            var html = @"<html prefix='og: http://ogp.me/ns#'>
+<head>
+<meta property='og:type' content='music.song' />
+<meta property='music:album:disc' content='55301' />
+</head>
+</html>";
+
+            var target = Graph.FromXml(html);
+
+            Assert.NotNull(target.MusicSong);
+            Assert.Equal(new[] { 55301 }, target.MusicSong.Albums.Select(a => a.Disc));
+        }
+
+        [Fact]
+        public void TrackTest()
+        {
+            var html = @"<html prefix='og: http://ogp.me/ns#'>
+<head>
+<meta property='og:type' content='music.song' />
+<meta property='music:album:track' content='55301' />
+</head>
+</html>";
+
+            var target = Graph.FromXml(html);
+
+            Assert.NotNull(target.MusicSong);
+            Assert.Equal(new[] { 55301 }, target.MusicSong.Albums.Select(a => a.Track));
+        }
+    }
+}
