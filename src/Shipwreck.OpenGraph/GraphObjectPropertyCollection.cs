55d34b41da7e10c02cfe03e40fe01cfd6ffa664a
pgrho <shipwreck@live.jp>
2017-08-19 10:04:22 +0900

    Split GraphObjectPropertyCollection to generic base collection

--- src/Shipwreck.OpenGraph/GraphObjectPropertyCollection.cs
+++ src/Shipwreck.OpenGraph/GraphObjectPropertyCollection.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Shipwreck.OpenGraph
 {
-    public sealed class GraphObjectPropertyCollection : IList<string>, IReadOnlyList<string>
+    public sealed class GraphObjectPropertyCollection : PartialCollection<string, GraphProperty>, IList<string>, IReadOnlyList<string>
     {
         private readonly GraphObject _Object;
         private readonly string _Property;
@@ -16,183 +13,19 @@ namespace Shipwreck.OpenGraph
             _Property = property;
         }
 
-        public string this[int index]
-        {
-            get => this.ElementAt(index);
-            set
-            {
-                var props = _Object._LocalProperties;
+        internal override IList<GraphProperty> PeekInternalList()
+            => _Object._LocalProperties;
 
-                var j = 0;
-                if (props != null)
-                {
-                    for (int i = 0; i < props.Count; i++)
-                    {
-                        var kv = props[i];
-                        if (kv.Property.MachesChildPath(_Object.Path, _Property) && j++ == index)
-                        {
-                            props[i] = new GraphProperty(_Object.Path + ":" + _Property, value);
-                            return;
-                        }
-                    }
-                }
-                if (index == j)
-                {
-                    _Object.LocalProperties.Add(new GraphProperty(_Object.Path + ":" + _Property, value));
-                }
-                else
-                {
-                    throw new IndexOutOfRangeException();
-                }
-            }
-        }
+        internal override IList<GraphProperty> GetOrCreateInternalList()
+            => _Object.LocalProperties;
 
-        public int Count
-            => _Object._LocalProperties == null ? 0 : this.Count();
+        internal override bool ShouldInclude(GraphProperty internalItem)
+            => internalItem.Property.MachesChildPath(_Object.Path, _Property);
 
-        bool ICollection<string>.IsReadOnly
-          => false;
+        internal override string ToItem(GraphProperty internalItem)
+            => internalItem.Content;
 
-        public void Add(string item)
-        {
-            if (item == null)
-            {
-                throw new ArgumentNullException(nameof(item));
-            }
-
-            _Object.LocalProperties.Add(new GraphProperty(_Object.Path + ":" + _Property, item));
-        }
-
-        public void Clear()
-            => _Object.SetLocalProperty(_Property, (string)null);
-
-        public bool Contains(string item)
-            => IndexOf(item) >= 0;
-
-        public void CopyTo(string[] array, int arrayIndex)
-        {
-            var props = _Object._LocalProperties;
-
-            if (props != null)
-            {
-                foreach (var kv in props)
-                {
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property))
-                    {
-                        array[arrayIndex++] = kv.Content;
-                    }
-                }
-            }
-        }
-
-        public IEnumerator<string> GetEnumerator()
-        {
-            var props = _Object._LocalProperties;
-
-            if (props != null)
-            {
-                foreach (var kv in props)
-                {
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property))
-                    {
-                        yield return kv.Content;
-                    }
-                }
-            }
-        }
-
-        public int IndexOf(string item)
-        {
-            var props = _Object._LocalProperties;
-
-            var i = 0;
-            if (props != null)
-            {
-                foreach (var kv in props)
-                {
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property))
-                    {
-                        if (kv.Content == item)
-                        {
-                            return i;
-                        }
-                        i++;
-                    }
-                }
-            }
-            return -1;
-        }
-
-        public void Insert(int index, string item)
-        {
-            var props = _Object._LocalProperties;
-
-            var j = 0;
-            if (props != null)
-            {
-                for (int i = 0; i < props.Count; i++)
-                {
-                    var kv = props[i];
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property) && j++ == index)
-                    {
-                        props.Insert(i, new GraphProperty(_Object.Path + ":" + _Property, item));
-                        return;
-                    }
-                }
-            }
-            if (index == j)
-            {
-                _Object.LocalProperties.Add(new GraphProperty(_Object.Path + ":" + _Property, item));
-            }
-            else
-            {
-                throw new IndexOutOfRangeException();
-            }
-        }
-
-        public bool Remove(string item)
-        {
-            var props = _Object._LocalProperties;
-
-            if (props != null)
-            {
-                for (int i = 0; i < props.Count; i++)
-                {
-                    var kv = props[i];
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property) && kv.Content == item)
-                    {
-                        props.RemoveAt(i);
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public void RemoveAt(int index)
-        {
-            var props = _Object._LocalProperties;
-
-            var j = 0;
-            if (props != null)
-            {
-                for (int i = 0; i < props.Count; i++)
-                {
-                    var kv = props[i];
-                    if (kv.Property.MachesChildPath(_Object.Path, _Property))
-                    {
-                        if (j == index)
-                        {
-                            props.RemoveAt(i);
-                            return;
-                        }
-                        j++;
-                    }
-                }
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-            => GetEnumerator();
+        internal override GraphProperty ToInternalItem(string item)
+            => new GraphProperty(_Object.Path + ":" + _Property, item);
     }
 }
---
+++ src/Shipwreck.OpenGraph/PartialCollection.cs
@@ -0,0 +1,242 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shipwreck.OpenGraph
+{
+    public abstract class PartialCollection<TItem, TInternal> : IList<TItem>, IReadOnlyCollection<TItem>
+    {
+        internal PartialCollection()
+        {
+            Comparer = EqualityComparer<TItem>.Default;
+        }
+
+        public IEqualityComparer<TItem> Comparer { get; }
+
+        #region Template Methods
+
+        internal abstract IList<TInternal> PeekInternalList();
+
+        internal abstract IList<TInternal> GetOrCreateInternalList();
+
+        internal abstract bool ShouldInclude(TInternal internalItem);
+
+        internal abstract TItem ToItem(TInternal internalItem);
+
+        internal abstract TInternal ToInternalItem(TItem item);
+
+        #endregion Template Methods
+
+        #region ICollection<T> Properties
+
+        public int Count
+            => PeekInternalList() == null ? 0 : this.Count();
+
+        bool ICollection<TItem>.IsReadOnly
+          => false;
+
+        #endregion ICollection<T> Properties
+
+        #region IList<T> Properties
+
+        public TItem this[int index]
+        {
+            get => this.ElementAt(index);
+            set
+            {
+                var props = PeekInternalList();
+
+                var j = 0;
+                if (props != null)
+                {
+                    for (int i = 0; i < props.Count; i++)
+                    {
+                        var kv = props[i];
+                        if (ShouldInclude(kv) && j++ == index)
+                        {
+                            props[i] = ToInternalItem(value);
+                            return;
+                        }
+                    }
+                }
+                if (index == j)
+                {
+                    GetOrCreateInternalList().Add(ToInternalItem(value));
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        #endregion IList<T> Properties
+
+        #region IEnumerable<T> Methods
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            var props = PeekInternalList();
+
+            if (props != null)
+            {
+                foreach (var kv in props)
+                {
+                    if (ShouldInclude(kv))
+                    {
+                        yield return ToItem(kv);
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        #endregion IEnumerable<T> Methods
+
+        #region ICollection<T> Methods
+
+        public void Add(TItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            GetOrCreateInternalList().Add(ToInternalItem(item));
+        }
+
+        public void Clear()
+        {
+            var prop = PeekInternalList();
+            if (prop != null)
+            {
+                for (var i = prop.Count - 1; i >= 0; i--)
+                {
+                    if (ShouldInclude(prop[i]))
+                    {
+                        prop.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        public bool Contains(TItem item)
+            => IndexOf(item) >= 0;
+
+        public void CopyTo(TItem[] array, int arrayIndex)
+        {
+            var props = PeekInternalList();
+
+            if (props != null)
+            {
+                foreach (var kv in props)
+                {
+                    if (ShouldInclude(kv))
+                    {
+                        array[arrayIndex++] = ToItem(kv);
+                    }
+                }
+            }
+        }
+
+        public bool Remove(TItem item)
+        {
+            var props = PeekInternalList();
+
+            if (props != null)
+            {
+                for (int i = 0; i < props.Count; i++)
+                {
+                    var kv = props[i];
+                    if (ShouldInclude(kv) && Comparer.Equals(ToItem(kv), item))
+                    {
+                        props.RemoveAt(i);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        #endregion ICollection<T> Methods
+
+        #region IList<T> Methods
+
+        public int IndexOf(TItem item)
+        {
+            var props = PeekInternalList();
+
+            var i = 0;
+            if (props != null)
+            {
+                foreach (var kv in props)
+                {
+                    if (ShouldInclude(kv))
+                    {
+                        if (Comparer.Equals(ToItem(kv), item))
+                        {
+                            return i;
+                        }
+                        i++;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, TItem item)
+        {
+            var props = PeekInternalList();
+
+            var j = 0;
+            if (props != null)
+            {
+                for (int i = 0; i < props.Count; i++)
+                {
+                    var kv = props[i];
+                    if (ShouldInclude(kv) && j++ == index)
+                    {
+                        props.Insert(i, ToInternalItem(item));
+                        return;
+                    }
+                }
+            }
+            if (index == j)
+            {
+                GetOrCreateInternalList().Add(ToInternalItem(item));
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            var props = PeekInternalList();
+
+            var j = 0;
+            if (props != null)
+            {
+                for (int i = 0; i < props.Count; i++)
+                {
+                    var kv = props[i];
+                    if (ShouldInclude(kv))
+                    {
+                        if (j == index)
+                        {
+                            props.RemoveAt(i);
+                            return;
+                        }
+                        j++;
+                    }
+                }
+            }
+        }
+
+        #endregion IList<T> Methods
+    }
+}
